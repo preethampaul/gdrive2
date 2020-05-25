ab30190011364bc7b78346d4085005ac2ddf0b85
Preetham Paul <preeth@uw.edu>
2020-05-25 02:44:42 -0700

    Changed gdrive/__init__.py to create gd

--- gdrive/__init__.py
+++ gdrive/__init__.py
@@ -1,1 +1,5 @@
 from .gd import *
+
+ROOT_PATH = os.path.dirname(__file__)
+if not 'gd' in os.listdir(ROOT_PATH):
+	shutil.copyfile(os.path.join(ROOT_PATH, 'gd.py'), os.path.join(ROOT_PATH, 'gd'))
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gdrive",
-    version="0.3.6",
+    version="0.3.7",
     author="Preetham Paul",
     author_email="preeth@uw.edu",
     description="File management tools for Google Drive based on PyDrive",
