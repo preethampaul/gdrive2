8e86f337d29c04db4350e7cd660661bb4b052cdb
Preetham Paul Sunkari <30369798+preethampaul@users.noreply.github.com>
2020-06-08 16:55:21 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Preetham Paul'
 author = 'Preetham Paul'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.2'
+release = '0.4.5'
 
 
 # -- General configuration ---------------------------------------------------
