7ba500fdf89163216f6f90311929067f0557e655
Preetham Paul <preeth@uw.edu>
2020-06-09 10:58:17 -0700

    updated to 0.4.7

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Preetham Paul'
 author = 'Preetham Paul'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.6'
+release = '0.4.7'
 
 
 # -- General configuration ---------------------------------------------------
